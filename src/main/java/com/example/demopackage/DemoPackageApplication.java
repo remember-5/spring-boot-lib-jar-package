73e0210250624a48542dad97c7d92de36291666e
wangjiahao <1332661444@qq.com>
2022-09-21 20:19:43 +0800

    Initial commit

---
+++ src/main/java/com/example/demopackage/DemoPackageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPackageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPackageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demopackage/DemoPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPackageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
